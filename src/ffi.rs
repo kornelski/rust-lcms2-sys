9b1bb834daf9aa14882d2460ebaffbb9e051d687
Kornel <kornel@geekhood.net>
2017-06-12 18:24:05 +0100

    Moved impls

--- src/ffi.rs
+++ src/ffi.rs
@@ -1346,58 +1346,6 @@ pub fn FLAGS_GRIDPOINTS(n: u32) -> u32 { ((n) & 0xFF) << 16 }
 /// CRD special
 pub const FLAGS_NODEFAULTRESOURCEDEF: u32 =     0x01000000;
 
-impl CIEXYZ {
-    pub fn d50() -> &'static CIEXYZ {
-        unsafe { cmsD50_XYZ() }
-    }
-}
-
-impl CIExyY {
-    pub fn d50() -> &'static CIExyY {
-        unsafe { cmsD50_xyY() }
-    }
-}
-
-impl From<CIEXYZ> for CIExyY {
-    fn from(f: CIEXYZ) -> Self {
-        unsafe {
-            let mut new = mem::uninitialized();
-            cmsXYZ2xyY(&mut new, &f);
-            new
-        }
-    }
-}
-
-impl From<CIExyY> for CIEXYZ {
-    fn from(f: CIExyY) -> Self {
-        unsafe {
-            let mut new = mem::uninitialized();
-            cmsxyY2XYZ(&mut new, &f);
-            new
-        }
-    }
-}
-
-impl From<CIELab> for CIELCh {
-    fn from(f: CIELab) -> Self {
-        unsafe {
-            let mut new = mem::uninitialized();
-            cmsLab2LCh(&mut new, &f);
-            new
-        }
-    }
-}
-
-impl From<CIELCh> for CIELab {
-    fn from(f: CIELCh) -> Self {
-        unsafe {
-            let mut new = mem::uninitialized();
-            cmsLCh2Lab(&mut new, &f);
-            new
-        }
-    }
-}
-
 #[derive(Copy, Clone, PartialEq, Eq)]
 #[repr(u32)]
 #[derive(Debug)]
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,62 @@
 
 pub mod ffi;
 pub use ffi::*;
+use std::mem;
 
 #[doc(hidden)]
 extern crate libc;
+
+impl CIEXYZ {
+    pub fn d50() -> &'static CIEXYZ {
+        unsafe { cmsD50_XYZ() }
+    }
+}
+
+impl CIExyY {
+    pub fn d50() -> &'static CIExyY {
+        unsafe { cmsD50_xyY() }
+    }
+}
+
+impl From<CIEXYZ> for CIExyY {
+    fn from(f: CIEXYZ) -> Self {
+        unsafe {
+            let mut new = mem::uninitialized();
+            cmsXYZ2xyY(&mut new, &f);
+            new
+        }
+    }
+}
+
+impl From<CIExyY> for CIEXYZ {
+    fn from(f: CIExyY) -> Self {
+        unsafe {
+            let mut new = mem::uninitialized();
+            cmsxyY2XYZ(&mut new, &f);
+            new
+        }
+    }
+}
+
+impl From<CIELab> for CIELCh {
+    fn from(f: CIELab) -> Self {
+        unsafe {
+            let mut new = mem::uninitialized();
+            cmsLab2LCh(&mut new, &f);
+            new
+        }
+    }
+}
+
+impl From<CIELCh> for CIELab {
+    fn from(f: CIELCh) -> Self {
+        unsafe {
+            let mut new = mem::uninitialized();
+            cmsLCh2Lab(&mut new, &f);
+            new
+        }
+    }
+}
 
 #[test]
 fn it_works() {

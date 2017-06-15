f54d6f73fab4ace80de5f2bdad378f44e3dcceb5
Kornel <kornel@geekhood.net>
2017-06-15 22:50:49 +0100

    Bundle LCMS2 for static build on Windows

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Makes [LCMS C API](https://github.com/mm2/Little-CMS) available in Rust. There's also a [higher-level wrapper](https://github.com/pornel/rust-lcms2).
 
-It's up to date with version 2.8.
+It's up to date with version 2.8. Linux, macOS and Windows are supported. Other platforms may work, too.
 
 ## Differences from [the C API](https://pornel.github.io/rust-lcms2-sys/)
 
@@ -10,3 +10,9 @@ It's up to date with version 2.8.
      * All C function names remained the same (with the prefix, e.g. `fn cmsReadTag()`).
  * Enum values don't have the `cmsSig` prefix, e.g. `cmsSigLabData` is `LabData`.
  * Some arguments use more specific type, e.g. `Intent::Perceptual` enum instead of `INTENT_PERCEPTUAL` integer.
+
+## Dynamic vs static
+
+The package supports "static" and "dynamic" [Cargo features](http://doc.crates.io/manifest.html#usage-in-end-products). If "dynamic" is selected (the default) then it will link to system-wide LCMS2 shared library if `pkg-config` is installed and working correctly.
+
+If `pkg-config` doesn't work (i.e. on Windows), or the "static" feature is enabled, it will build LCMS 2.8 from source instead.
--- src/build.rs
+++ src/build.rs
@@ -1,4 +1,7 @@
+#[cfg(feature = "dynamic")]
 extern crate pkg_config;
+#[cfg(feature = "static")]
+extern crate gcc;
 
 use std::env;
 
@@ -11,11 +14,67 @@ fn main() {
         println!("cargo:include={}", include_dir);
     }
 
-    if let Ok(info) = pkg_config::find_library("lcms2") {
-        for path in info.include_paths {
-            println!("cargo:include={}", path.display());
-        }
-    } else {
-        println!("cargo:rustc-link-lib=lcms2");
+    if cfg!(feature = "static") || !configure_dynamic() {
+        compile_static();
     }
+}
+
+#[cfg(feature = "dynamic")]
+fn configure_dynamic() -> bool {
+    match pkg_config::find_library("lcms2") {
+        Ok(info) => {
+            for path in info.include_paths {
+                println!("cargo:include={}", path.display());
+            }
+            true
+        },
+        Err(err) => {
+            println!("cargo:warning=pkg_config failed ({}). Falling back to static build.", err);
+            false
+        },
+    }
+}
+
+#[cfg(not(feature = "dynamic"))]
+fn configure_dynamic() -> bool {
+    false
+}
+
+#[cfg(not(feature = "static"))]
+fn compile_static() {
+    println!("cargo:warning='static' feature of lcms2-sys is disabled, so the library won't be built, and probably won't work at all");
+    println!("cargo:rustc-link-lib=lcms2");
+}
+
+#[cfg(feature = "static")]
+fn compile_static() {
+    gcc::Config::new()
+        .include("vendor/include")
+        .file("vendor/src/cmsalpha.c")
+        .file("vendor/src/cmscam02.c")
+        .file("vendor/src/cmscgats.c")
+        .file("vendor/src/cmscnvrt.c")
+        .file("vendor/src/cmserr.c")
+        .file("vendor/src/cmsgamma.c")
+        .file("vendor/src/cmsgmt.c")
+        .file("vendor/src/cmshalf.c")
+        .file("vendor/src/cmsintrp.c")
+        .file("vendor/src/cmsio0.c")
+        .file("vendor/src/cmsio1.c")
+        .file("vendor/src/cmslut.c")
+        .file("vendor/src/cmsmd5.c")
+        .file("vendor/src/cmsmtrx.c")
+        .file("vendor/src/cmsnamed.c")
+        .file("vendor/src/cmsopt.c")
+        .file("vendor/src/cmspack.c")
+        .file("vendor/src/cmspcs.c")
+        .file("vendor/src/cmsplugin.c")
+        .file("vendor/src/cmsps2.c")
+        .file("vendor/src/cmssamp.c")
+        .file("vendor/src/cmssm.c")
+        .file("vendor/src/cmstypes.c")
+        .file("vendor/src/cmsvirt.c")
+        .file("vendor/src/cmswtpnt.c")
+        .file("vendor/src/cmsxform.c")
+        .compile("liblcms2.a");
 }
